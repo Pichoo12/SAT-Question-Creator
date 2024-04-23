b3cdf12426306d95089393d69d81d1f552c98e70
Pichoo12 <s1802049@online.houstonisd.org>
2024-04-23 11:00:51 -0500

    init

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class Game:
         self.screen_height = height
 
     def run(self):
+        
         pygame.init()
         screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         pygame.display.set_caption("My Game")
@@ -18,18 +19,17 @@ class Game:
                 if event.type == pygame.QUIT:
                     running = False
 
-            screen.fill((255, 255, 255))  # Fill the screen with white color
+            screen.fill((255, 255, 255))  
 
-            # Update the display
+           
             pygame.display.flip()
 
-            # Control frame rate
+            
             clock.tick(30)
 
-        # Quit pygame
+        
         pygame.quit()
 
-# Instantiate and run the game
 if __name__ == "__main__":
     game = Game()
     game.run()
